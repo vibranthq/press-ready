95c6f5c8a4f1a50f04c4f21e6d73f36c68b88d8c
Yasuaki Uechi <y@uechi.io>
2019-10-16 23:27:40 +0900

    fix: object spread

--- src/cli.js
+++ src/cli.js
@@ -10,200 +10,200 @@ const { ghostScript } = require('./ghostScript')
 
 const ASSETS_DIR = path.resolve(__dirname, '..', 'assets')
 const tableArgs = {
-  chars: {
-    top: '',
-    'top-mid': '',
-    'top-left': '',
-    'top-right': '',
-    bottom: '',
-    'bottom-mid': '',
-    'bottom-left': '',
-    'bottom-right': '',
-    left: '',
-    'left-mid': '',
-    mid: '',
-    'mid-mid': '',
-    right: '',
-    'right-mid': '',
-    middle: ' ',
-  },
-  style: { 'padding-left': 0, head: ['white'] },
+    chars: {
+        top: '',
+        'top-mid': '',
+        'top-left': '',
+        'top-right': '',
+        bottom: '',
+        'bottom-mid': '',
+        'bottom-left': '',
+        'bottom-right': '',
+        left: '',
+        'left-mid': '',
+        mid: '',
+        'mid-mid': '',
+        right: '',
+        'right-mid': '',
+        middle: ' ',
+    },
+    style: { 'padding-left': 0, head: ['white'] },
 }
 
 function log(...obj) {
-  // eslint-disable-next-line no-console
-  console.log(chalk.gray('==>'), chalk.blue(...obj))
+    // eslint-disable-next-line no-console
+    console.log(chalk.gray('==>'), chalk.blue(...obj))
 }
 
 function rawLog(...obj) {
-  // eslint-disable-next-line no-console
-  console.log(...obj)
+    // eslint-disable-next-line no-console
+    console.log(...obj)
 }
 
 async function inspectPDF(filePath) {
-  const { fonts } = await pdfFonts(filePath)
+    const { fonts } = await pdfFonts(filePath)
 
-  if (fonts.length) {
-    const table = new Table({
-      head: ['name', 'type', 'embedded', 'subset'],
-      ...tableArgs,
-    })
-    for (const font of fonts) {
-      table.push([
-        chalk.gray(font.name),
-        chalk.yellow(font.type),
-        font.emb === 'yes' ? chalk.green('yes') : chalk.red('no'),
-        font.sub === 'yes' ? chalk.green('yes') : chalk.red('no'),
-      ])
+    if (fonts.length) {
+        const table = new Table({
+            head: ['name', 'type', 'embedded', 'subset'],
+            ...tableArgs,
+        })
+        for (const font of fonts) {
+            table.push([
+                chalk.gray(font.name),
+                chalk.yellow(font.type),
+                font.emb === 'yes' ? chalk.green('yes') : chalk.red('no'),
+                font.sub === 'yes' ? chalk.green('yes') : chalk.red('no'),
+            ])
+        }
+        rawLog(table.toString())
+    } else {
+        log(chalk.yellow('No fonts found'))
     }
-    rawLog(table.toString())
-  } else {
-    log(chalk.yellow('No fonts found'))
-  }
 
-  // Check if all fonts are embedded
-  const isEmbedded = fonts.every((font) => font.emb === 'yes')
+    // Check if all fonts are embedded
+    const isEmbedded = fonts.every((font) => font.emb === 'yes')
 
-  // Check if all fonts are a subset of font
-  const isSubset = fonts.every((font) => font.sub === 'yes')
+    // Check if all fonts are a subset of font
+    const isSubset = fonts.every((font) => font.sub === 'yes')
 
-  const shouldEnforceOutline = !isEmbedded || !isSubset
-  if (shouldEnforceOutline) {
-    log(chalk.red('Some fonts need to be outlined'))
-  } else {
-    log(chalk.green('Every font is properly embedded'))
-  }
+    const shouldEnforceOutline = !isEmbedded || !isSubset
+    if (shouldEnforceOutline) {
+        log(chalk.red('Some fonts need to be outlined'))
+    } else {
+        log(chalk.green('Every font is properly embedded'))
+    }
 
-  return {
-    isEmbedded,
-    isSubset,
-    shouldEnforceOutline,
-  }
+    return {
+        isEmbedded,
+        isSubset,
+        shouldEnforceOutline,
+    }
 }
 
 async function build(args) {
-  if (!args.input || !args.output) {
-    throw new Error('No input given')
-  }
-
-  const resolvedInput = path.resolve(args.input)
-  const resolvedOutput = path.resolve(args.output)
-
-  log(`Listing fonts in '${args.input}'`)
-  const { shouldEnforceOutline } = await inspectPDF(resolvedInput)
-  const isEnforceOutline =
-    args.enforceOutline !== undefined
-      ? args.enforceOutline
-      : shouldEnforceOutline
-
-  log('Generating PDF (using Ghostscript)')
-  const table = new Table(tableArgs)
-  table.push(
-    {
-      Input: chalk.white(args.input),
-    },
-    {
-      Output: chalk.white(args.output),
-    },
-    {
-      'Color Mode': args.grayScale
-        ? chalk.white('Gray')
-        : `${chalk.cyan('C')}${chalk.red('M')}${chalk.yellow('Y')}${chalk.white(
-            'K'
-          )}`,
-    },
-    {
-      'Enforce outline': isEnforceOutline
-        ? chalk.green('yes')
-        : chalk.red('no'),
-    },
-    {
-      'Boundary boxes': args.boundaryBoxes
-        ? chalk.green('yes')
-        : chalk.red('no'),
+    if (!args.input || !args.output) {
+        throw new Error('No input given')
     }
-  )
-  rawLog(table.toString())
 
-  const gsResult = await ghostScript(
-    resolvedInput,
-    resolvedOutput,
-    path.join(ASSETS_DIR, 'PDFX_def.ps.mustache'),
-    path.join(ASSETS_DIR, 'JapanColor2001Coated.icc'),
-    args.grayScale,
-    isEnforceOutline,
-    args.boundaryBoxes
-  )
+    const resolvedInput = path.resolve(args.input)
+    const resolvedOutput = path.resolve(args.output)
 
-  if (gsResult.rawError) {
-    log(chalk.red('Done with some errors'))
-    rawLog(chalk.gray(gsResult.rawError))
-  } else {
-    log('Done without error')
-  }
+    log(`Listing fonts in '${args.input}'`)
+    const { shouldEnforceOutline } = await inspectPDF(resolvedInput)
+    const isEnforceOutline =
+        args.enforceOutline !== undefined
+            ? args.enforceOutline
+            : shouldEnforceOutline
 
-  log(`Listing fonts in '${args.output}'`)
-  await inspectPDF(resolvedOutput)
+    log('Generating PDF (using Ghostscript)')
+    const table = new Table(tableArgs)
+    table.push(
+        {
+            Input: chalk.white(args.input),
+        },
+        {
+            Output: chalk.white(args.output),
+        },
+        {
+            'Color Mode': args.grayScale
+                ? chalk.white('Gray')
+                : `${chalk.cyan('C')}${chalk.red('M')}${chalk.yellow(
+                      'Y'
+                  )}${chalk.white('K')}`,
+        },
+        {
+            'Enforce outline': isEnforceOutline
+                ? chalk.green('yes')
+                : chalk.red('no'),
+        },
+        {
+            'Boundary boxes': args.boundaryBoxes
+                ? chalk.green('yes')
+                : chalk.red('no'),
+        }
+    )
+    rawLog(table.toString())
+
+    const gsResult = await ghostScript(
+        resolvedInput,
+        resolvedOutput,
+        path.join(ASSETS_DIR, 'PDFX_def.ps.mustache'),
+        path.join(ASSETS_DIR, 'JapanColor2001Coated.icc'),
+        args.grayScale,
+        isEnforceOutline,
+        args.boundaryBoxes
+    )
+
+    if (gsResult.rawError) {
+        log(chalk.red('Done with some errors'))
+        rawLog(chalk.gray(gsResult.rawError))
+    } else {
+        log('Done without error')
+    }
+
+    log(`Listing fonts in '${args.output}'`)
+    await inspectPDF(resolvedOutput)
 }
 
 async function lint(args) {
-  const info = await pdfInfo(args.input)
-  log(`Linting metadata for '${args.input}'`)
-  log(chalk.gray('Title'), info.Title)
-  log(chalk.gray('Page No.'), info.Pages)
-  log(chalk.gray('PDF version'), info['PDF version'])
-  log(chalk.gray('TrimBox'), Object.values(info.TrimBox))
-  log(chalk.gray('BleedBox'), Object.values(info.BleedBox))
-  log('Listing fonts')
-  await inspectPDF(args.input)
+    const info = await pdfInfo(args.input)
+    log(`Linting metadata for '${args.input}'`)
+    log(chalk.gray('Title'), info.Title)
+    log(chalk.gray('Page No.'), info.Pages)
+    log(chalk.gray('PDF version'), info['PDF version'])
+    log(chalk.gray('TrimBox'), Object.values(info.TrimBox))
+    log(chalk.gray('BleedBox'), Object.values(info.BleedBox))
+    log('Listing fonts')
+    await inspectPDF(args.input)
 }
 
 yargs
-  .scriptName('press-ready')
-  .command(
-    ['build', '$0'],
-    'build PDF',
-    (yargs) =>
-      yargs
-        .option('input', {
-          demandOption: true,
-          alias: 'i',
-          description: 'Input file path (relative)',
-        })
-        .option('output', {
-          default: './output.pdf',
-          alias: 'o',
-          description: 'Output file path (relative)',
-        })
-        .option('gray-scale', {
-          boolean: true,
-          default: false,
-          description: 'Use gray scale color space instead of CMYK',
-        })
-        .option('enforce-outline', {
-          boolean: true,
-          description: 'Convert embedded fonts to outlined fonts',
-        })
-        .option('boundary-boxes', {
-          boolean: true,
-          default: false,
-          description: 'Add boundary boxes on every page',
-        }),
-    build
-  )
-  .command(
-    ['lint'],
-    'lint PDF',
-    (yargs) =>
-      yargs.option('input', {
-        required: true,
-        alias: 'i',
-        description: 'Input file path (relative)',
-      }),
-    lint
-  )
-  .fail((msg, err) => {
-    log(chalk.red(msg || err))
-    process.exit()
-  })
-  .help().argv
+    .scriptName('press-ready')
+    .command(
+        ['build', '$0'],
+        'build PDF',
+        (yargs) =>
+            yargs
+                .option('input', {
+                    demandOption: true,
+                    alias: 'i',
+                    description: 'Input file path (relative)',
+                })
+                .option('output', {
+                    default: './output.pdf',
+                    alias: 'o',
+                    description: 'Output file path (relative)',
+                })
+                .option('gray-scale', {
+                    boolean: true,
+                    default: false,
+                    description: 'Use gray scale color space instead of CMYK',
+                })
+                .option('enforce-outline', {
+                    boolean: true,
+                    description: 'Convert embedded fonts to outlined fonts',
+                })
+                .option('boundary-boxes', {
+                    boolean: true,
+                    default: false,
+                    description: 'Add boundary boxes on every page',
+                }),
+        build
+    )
+    .command(
+        ['lint'],
+        'lint PDF',
+        (yargs) =>
+            yargs.option('input', {
+                required: true,
+                alias: 'i',
+                description: 'Input file path (relative)',
+            }),
+        lint
+    )
+    .fail((msg, err) => {
+        log(chalk.red(msg || err))
+        process.exit()
+    })
+    .help().argv
--- src/ghostScript.js
+++ src/ghostScript.js
@@ -6,82 +6,82 @@ const Mustache = require('mustache')
 const debug = require('debug')('press-ready')
 
 async function ghostScript(
-  inputPath,
-  outputPath,
-  pdfxDefTemplatePath,
-  iccProfilePath,
-  grayScale = false,
-  enforceOutline = false,
-  boundaryBoxes = false
-) {
-  const pdfxDefPath = join(tmpdir(), 'press-ready-def.ps')
-  const gsCommand = 'gs'
-  const gsOptions = [
-    '-dPDFX',
-    '-dBATCH',
-    '-dNOPAUSE',
-    '-dNOOUTERSAVE',
-    '-sDEVICE=pdfwrite',
-    '-dPDFSTOPONERROR',
-    '-dShowAnnots=false',
-    '-dPDFSETTINGS=/prepress',
-    '-dPrinted',
-    '-r600',
-    '-dGrayImageResolution=600',
-    '-dMonoImageResolution=600',
-    '-dColorImageResolution=600',
-    `-sOutputFile=${outputPath}`,
-  ]
-  if (boundaryBoxes) {
-    gsOptions.push('-dUseCropBox', '-dUseTrimBox', '-dUseBleedBox')
-  }
-  if (enforceOutline) {
-    gsOptions.push('-dNoOutputFonts')
-  }
-  if (grayScale) {
-    gsOptions.push(
-      '-sProcessColorModel=DeviceGray',
-      '-sColorConversionStrategy=Gray',
-      '-sColorConversionStrategyForImages=Gray'
-    )
-  } else {
-    gsOptions.push(
-      '-sProcessColorModel=DeviceCMYK',
-      '-sColorConversionStrategy=CMYK',
-      '-sColorConversionStrategyForImages=CMYK',
-      '-dOverrideICC',
-      `-sOutputICCProfile=${iccProfilePath}`
-    )
-  }
-
-  // generate PDFX_def.ps
-  const pdfxDefTemplate = fs.readFileSync(pdfxDefTemplatePath, 'utf-8')
-  const pdfxDef = Mustache.render(pdfxDefTemplate, {
-    title: 'Auto-generated PDF (press-ready)',
+    inputPath,
+    outputPath,
+    pdfxDefTemplatePath,
     iccProfilePath,
-  })
-  fs.writeFileSync(pdfxDefPath, pdfxDef, 'utf-8')
+    grayScale = false,
+    enforceOutline = false,
+    boundaryBoxes = false
+) {
+    const pdfxDefPath = join(tmpdir(), 'press-ready-def.ps')
+    const gsCommand = 'gs'
+    const gsOptions = [
+        '-dPDFX',
+        '-dBATCH',
+        '-dNOPAUSE',
+        '-dNOOUTERSAVE',
+        '-sDEVICE=pdfwrite',
+        '-dPDFSTOPONERROR',
+        '-dShowAnnots=false',
+        '-dPDFSETTINGS=/prepress',
+        '-dPrinted',
+        '-r600',
+        '-dGrayImageResolution=600',
+        '-dMonoImageResolution=600',
+        '-dColorImageResolution=600',
+        `-sOutputFile=${outputPath}`,
+    ]
+    if (boundaryBoxes) {
+        gsOptions.push('-dUseCropBox', '-dUseTrimBox', '-dUseBleedBox')
+    }
+    if (enforceOutline) {
+        gsOptions.push('-dNoOutputFonts')
+    }
+    if (grayScale) {
+        gsOptions.push(
+            '-sProcessColorModel=DeviceGray',
+            '-sColorConversionStrategy=Gray',
+            '-sColorConversionStrategyForImages=Gray'
+        )
+    } else {
+        gsOptions.push(
+            '-sProcessColorModel=DeviceCMYK',
+            '-sColorConversionStrategy=CMYK',
+            '-sColorConversionStrategyForImages=CMYK',
+            '-dOverrideICC',
+            `-sOutputICCProfile=${iccProfilePath}`
+        )
+    }
 
-  // generate pdf with ghostscript
-  const args = [...gsOptions, pdfxDefPath, inputPath]
-  const command = [gsCommand, args]
-  debug(command)
-  try {
-    const { stdout, stderr } = await execa(...command)
-    return {
-      command,
-      rawOutput: stdout,
-      rawError: stderr,
+    // generate PDFX_def.ps
+    const pdfxDefTemplate = fs.readFileSync(pdfxDefTemplatePath, 'utf-8')
+    const pdfxDef = Mustache.render(pdfxDefTemplate, {
+        title: 'Auto-generated PDF (press-ready)',
+        iccProfilePath,
+    })
+    fs.writeFileSync(pdfxDefPath, pdfxDef, 'utf-8')
+
+    // generate pdf with ghostscript
+    const args = [...gsOptions, pdfxDefPath, inputPath]
+    const command = [gsCommand, args]
+    debug(command)
+    try {
+        const { stdout, stderr } = await execa(...command)
+        return {
+            command,
+            rawOutput: stdout,
+            rawError: stderr,
+        }
+    } catch (err) {
+        return {
+            command,
+            rawOutput: err.stdout,
+            rawError: err.stderr,
+        }
     }
-  } catch (err) {
-    return {
-      command,
-      rawOutput: err.stdout,
-      rawError: err.stderr,
-    }
-  }
 }
 
 module.exports = {
-  ghostScript,
+    ghostScript,
 }
