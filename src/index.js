73b39e73858900e92dcc402d008ac995b4a44ace
Yasuaki Uechi <y@uechi.io>
2020-01-05 15:53:48 +0900

    feat: export commands

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 export {pdfFonts, pdfInfo, isXPDFAvailable} from './xpdf';
 export {ghostScript, isGhostscriptAvailable} from './ghostScript';
+export {inspectPDF} from './inspectPDF';
+export {build} from './commands/build';
+export {lint} from './commands/lint';
